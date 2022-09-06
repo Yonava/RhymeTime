0eaaf7bad7f7dd581d042e1bbce160c0750bd94b
Yona Voss-Andreae <yona@Yonas-MacBook-Pro.local>
2022-09-06 15:20:02 -0400

    my first major contribution

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>Hi, this is yona!</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
 
