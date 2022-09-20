8a885486ca5abde3cccf68297a7ca5a5f3258040
Yona Voss-Andreae <yona@Yonas-MacBook-Pro.local>
2022-09-19 22:36:56 -0400

    serving statics

--- server/index.js
+++ server/index.js
@@ -47,5 +47,10 @@ app.get('/seesandwiches', (req, res) => {
   })
 })
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(__dirname + '/public/'));
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`)})
