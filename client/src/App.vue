559744c402063769534ca8dde1ee5c343b6b9928
yonava <yonavossandreae@gmail.com>
2022-09-25 16:20:07 -0400

    keyed

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <v-app>
-    <router-view/>
+    <router-view v-if="password === key" />
+    <center v-else>
+      <h1>RhymeTime Coming Soon</h1>
+    </center>
   </v-app>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      password: localStorage.password,
+      key: 'open sesame'
+    }
+  }
 }
 </script>
 
