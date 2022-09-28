b036cd0fcc573497928beb8501d7f2db631d0324
yonava <yonavossandreae@gmail.com>
2022-09-28 05:05:33 -0400

    added audio board

--- client/src/components/HostViews/HostMixin.js
+++ client/src/components/HostViews/HostMixin.js
@@ -7,8 +7,13 @@ export default {
       totalTime: 30,
       // time remaining in seconds until view has to be changed
       timeRemaining: 30,
+      // if false, timer will never be run on that component
+      timerDisabled: false,
       // true if an interval is actively decrementing timeRemaing
       timerRunning: false,
+      // stores audio instance for gameplay music
+      audio: undefined,
+      beepSound: new Audio(require('../../../assets/beep.mp3')),
 
       // for developing ui only. make sure this is set to false in prod
       testMode: false,
@@ -47,36 +52,48 @@ export default {
     // maybe a ts enum??? worth exploring!
     switch (this.$parent.currentView) {
       case 'respond':
-        this.timeRemaining = 90
+        this.timeRemaining = 10
+        this.audio = new Audio(require('../../../assets/respond.mp3'))
         break
       case 'vote':
         this.timeRemaining = 20
+        this.audio = new Audio(require('../../../assets/vote.mp3'))
         break
       case 'recap':
         this.timeRemaining = 10
+        this.audio = undefined
         break
       case 'waiting': 
-        return
+        this.audio = new Audio(require('../../../assets/waiting.mp3'))
+        break
       case 'intro':
-        return
+        this.audio = undefined
+        break
       case 'outro':
-        return
+        this.audio = undefined
+        break
       default:
         return console.error('Uncaught Case Passed Down to HostMixin')
     }
 
     this.totalTime = this.timeRemaining
+    if (this.audio?.play()) this.audio.play()
     if (!this.testMode) this.startTimer()
   },
   destroyed() {
     clearInterval(this.timer)
+    if (this.audio?.pause()) {
+      this.audio.pause()
+      this.audio = undefined
+    }
   },
   methods: {
     startTimer() {
-      if (this.timerRunning) return
+      if (this.timerRunning || this.timerDisabled) return
       this.timer = setInterval(() => {
         this.timeRemaining--
         if (this.timeRemaining < 0) this.next()
+        else if (this.timeRemaining < 5) this.playEffect('beep')
       }, 1000)
       this.timerRunning = true
     },
@@ -87,11 +104,19 @@ export default {
     },
     pauseGame() {
       this.stopTimer()
-      // add audio controls here
+      if (this.audio?.pause()) this.audio.pause()
     },
     unpauseGame() {
       if (!this.testMode) this.startTimer()
-      // add audio controls here
+      if (this.audio?.play()) this.audio.play()
+    },
+    playEffect(soundEffect) {
+      switch (soundEffect) {
+        case 'beep': {
+          this.beepSound.currentTime = 0
+          this.beepSound.play()
+        }
+      }
     }
   }
 }
