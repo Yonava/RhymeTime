79542509cb293b276262dfac24f249c8d9f5ccb0
Yonava <yonavossandreae@gmail.com>
2022-12-11 03:01:09 -0500

    pt3 tutorial frame complete

--- client/src/components/HostViews/Tutorial.vue
+++ client/src/components/HostViews/Tutorial.vue
@@ -100,7 +100,7 @@ export default {
         }
       ],
       // this is the index of the current frame
-      frameIndex: 2,
+      frameIndex: 0,
       // this holds the audio element for the tutorial voiceover
       voAudio: null,
       // this is the flag for whether the tutorial has started
